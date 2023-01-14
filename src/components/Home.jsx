fe96a65a620666e4858b53247bd9dc436158882f
Cameron <camshih@gmail.com>
2023-01-14 15:42:28 -0500

    new project

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+const Home = () => {
+  return (
+    <div className='h-screen grid place-items-center'>
+      <Navbar />
+      <div>
+        body
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './About'
+import Contact from './Contact';
+// import Home from './Home';
+
+const Navbar = () => {
+  return (
+    <div className=''>
+        <nav>
+            {/* <Link to="/">Home</Link> */}
+            <Link to="/about">About</Link>
+            <Link to="contact">Contact</Link>
+        </nav>
+        <Routes>
+            {/* <Route path="/" element={<Home />}/> */}
+            <Route path="/about" element={<About />}/>
+            <Route path="/contact/:email" element={<Contact />} />
+        </Routes> 
+    </div>
+  )
+}
+
+export default Navbar
