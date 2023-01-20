93bda8cdc63cf556a1c000c043fbe180d3919cee
Cameron <camshih@gmail.com>
2023-01-20 11:09:35 -0500

    div structure

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <BrowserRouter>
-    <div className='h-screen'>
-      <div className='pt-48 grid grid-cols-1 content-center place-items-center'>
+    <div className='p-4 relative'>
+      <div className='pt-48 px-72 grid grid-cols-1 content-center place-items-center'>
         <Navbar />
-        <div className='mt-8 grid grid-cols-2'>
-          <div>
+        <div className='w-full mt-8'>
+          I am currently a Computer Science Student at Toronto Metropolitan University. I have a passion for trying new things in and out of the tech world. During my personal time, you can find me at the gym playing volleyball or basketball, weightlifting, hanging out around the city or at home watching shows. 
+        </div>
+        <div className='w-full'>
+          aaa
+        </div>
+        <div className='w-full border-t-2 border-slate-300 text-left grid grid-cols-2 place-items-start content-start'>
+          <div className=''>
             left
           </div>
           <div>
@@ -17,7 +23,6 @@ function App() {
         </div>
       </div>
     </div>
-
     </BrowserRouter>
   );
 }
