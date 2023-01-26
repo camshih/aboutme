b7000180501383069f199382c8b25e2baaa3432f
Cameron <camshih@gmail.com>
2023-01-26 13:23:48 -0500

    moved footer to component + trim

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 function App() {
   return (
     <BrowserRouter>
-    <div className='p-4 relative'>
-      <div className='pt-48 px-72 grid grid-cols-1 content-center place-items-center'>
-        <Navbar />
-        <div className='w-full mt-8'>
-          I am currently a Computer Science Student at Toronto Metropolitan University. I have a passion for trying new things in and out of the tech world. During my personal time, you can find me at the gym playing volleyball or basketball, weightlifting, hanging out around the city or at home watching shows. 
-        </div>
-        <div className='w-full'>
-          aaa
-        </div>
-        <div className='w-full border-t-2 border-slate-300 text-left grid grid-cols-2 place-items-start content-start'>
-          <div className='space-x-2 text-blue-500 font-medium'>
-            <a href="https://www.linkedin.com/in/cameron-shih/">Linkedin</a>
-            <a href="https://github.com/camshih">Github</a>
-            <a href="">Resume</a>
-            <a href="mailto:camshih@gmail.com">Email</a>
-          </div>
-        </div>
+    <div className='py-4 px-12 lg:px-48 lg:py-16 relative'>
+      <div className='text-2xl sm:text-4xl'>
+        Cameron Shih
       </div>
+      <div className='py-2'>
+      I am currently a Computer Science Student at Toronto Metropolitan University. I have a passion for trying new things in and out of the tech world. During my personal time, you can find me at the gym playing volleyball or basketball, weightlifting, hanging out around the city or at home watching shows. 
+      </div>
+      <Footer />
     </div>
     </BrowserRouter>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='space-x-2 text-blue-500 font-medium border-t-2 border-slate-300'>
+      <a href="https://www.linkedin.com/in/cameron-shih/">Linkedin</a>
+      <a href="https://github.com/camshih">Github</a>
+      <a href="">Resume</a>
+      <a href="mailto:camshih@gmail.com">Email</a>
+    </div>
   )
 }
 
